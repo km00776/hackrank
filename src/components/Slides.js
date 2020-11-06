d87f03896dff2cd67391fd4dbb467f59b68c0edd
km00776 <kmurugathas@gmail.com>
2020-11-06 14:44:32 -0800

    question 1 of hackerrank complete@

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function Slides({slides}) {
+function Slides({ slides}) {
+  console.log(slides);
+  const [currentIndex, setIndex] = useState(0);
 
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
-
+ console.log(slides.length);
+  
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button data-testid="button-restart" className="small outlined" onClick={() => setIndex(0)}>
+          Restart
+        </button>
+        <button data-testid="button-prev" className="small" onClick={() => currentIndex > 0 ? setIndex(currentIndex - 1) : currentIndex}>
+          Prev
+        </button>
+        <button data-testid="button-next" className="small" onClick={() => currentIndex < 4 ? setIndex(currentIndex + 1) : currentIndex}>
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{slides[currentIndex].title}</h1>
+        <p data-testid="text">{slides[currentIndex].text}  </p>
+      </div>
+    </div>
+  );
 }
 
 export default Slides;
